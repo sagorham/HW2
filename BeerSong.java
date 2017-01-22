2b868f024a4cc7bcdd7b62400082bd6782a3549f
Stephanie <sagorham@oakland.edu>
2017-01-22 17:30:38 -0500

    Add BeerSong.java

---
+++ BeerSong.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beersong;
+
+/**
+ *
+ * @author Stephanie
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Ninety_Nine_Bottles_of_Beer(); 
+    }
+    
+    public static void Ninety_Nine_Bottles_of_Beer(){
+         
+        
+    }
+        
+        
+        
+    }
+}
+    
