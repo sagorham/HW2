5e07904d7fc4c75487d0873b8a96fe51b9f9b71d
Stephanie <sagorham@oakland.edu>
2017-01-22 19:08:14 -0500

    Added period after verses with additional spaces.

--- BeerSong.java
+++ BeerSong.java
@@ -19,21 +19,21 @@ public class BeerSong {
     }
     
     public static void Ninety_Nine_Bottles_of_Beer(int x){
-		
+ 
          if(x>1){
-            System.out.print(x +" bottles of beer on the wall," + x + " bottles of beer\n"); 
+            System.out.print(x +" bottles of beer on the wall ," + x + " bottles of beer\n"); 
              System.out.println("take one down, pass it around, " );
-            System.out.println((x-1)+" bottles of beer on the wall");
+            System.out.println((x-1)+" bottles of beer on the wall. ");
             Ninety_Nine_Bottles_of_Beer(x-1); //decrement the x value
         
     } //for the last verse of the song, only use singular bottle. 
          else if(x<=1){
-        System.out.println(x + " bottle of beer on the wall, "+ x + " bottle of beer "); 
+        System.out.println(x + " bottle of beer on the wall , "+ x + " bottle of beer "); 
         System.out.println("take one down pass it around, "); 
         System.out.println("no more bottles of beer on the wall."); 
+    }
         
-        
-        
+	
     }
 }
     
